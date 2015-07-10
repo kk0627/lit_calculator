687274de74c08287a5c5c23f6e4ede04d88b2225
keita kutsuno <keita@keita-no-MacBook-Air.local>
2015-07-11 00:24:49 +0900

    “小数点計算を実装”

--- lit_calculator/ViewController.swift
+++ lit_calculator/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
     var number : Double = 0
     var number2 : Double!
     var operation : Int = 0
+    var counts : Double = 10
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -26,18 +28,45 @@ class ViewController: UIViewController {
     }
     
     @IBAction func clickNumberButton(sender:UIButton) {
-        number = number*10 + Double(sender.tag)
-        doubleToInt()
+        if myLabel.tag == 1{
+            number = number + Double(sender.tag)/counts
+            counts = counts * 10
+            doubleToInt()
+        }else{
+            number = number*10 + Double(sender.tag)
+            doubleToInt()
+            myLabel.tag = 0
+        }
+        
+    }
+    
+    @IBAction func clickDotButton(){
+        
+        var numberString : String = String(stringInterpolationSegment: number)
+        numberString.hasSuffix("0")
+        var startIndex:String.Index
+        var endIndex:String.Index
+        //0文字目から
+        startIndex = advance(numberString.startIndex,0)
+        //末尾から2文字目(.0)まで
+        endIndex = advance(numberString.startIndex, count(numberString) - 1)
+        var newString = numberString.substringWithRange(Range(start:startIndex ,end:endIndex))
+        myLabel.text = newString
+        myLabel.tag = 1
+        
     }
     
     @IBAction func allClear(){
         number = 0
         doubleToInt()
+        myLabel.tag = 0
+        counts = 10
     }
     
     @IBAction func plus() {
         doubleToInt()
         number2 = number
+        myLabel.tag = 0
         operation = 1
         number = 0
     }
@@ -46,6 +75,7 @@ class ViewController: UIViewController {
         doubleToInt()
         number2 = number
         operation = 2
+        myLabel.tag = 0
         number = 0
     }
     
@@ -53,6 +83,7 @@ class ViewController: UIViewController {
         doubleToInt()
         number2 = number
         operation = 3
+        myLabel.tag = 0
         number = 0
     }
     
@@ -60,6 +91,7 @@ class ViewController: UIViewController {
         doubleToInt()
         number2 = number
         operation = 4
+        myLabel.tag = 0
         number = 0
     }
     
@@ -68,24 +100,29 @@ class ViewController: UIViewController {
             number = number + number2
             doubleToInt()
             operation = 0
+            myLabel.tag = 0
+            counts = 10
         }
         
         if operation == 2{
             number = number2 - number
             doubleToInt()
             operation = 0
+            myLabel.tag = 0
         }
         
         if operation == 3{
             number = number * number2
             doubleToInt()
             operation = 0
+            myLabel.tag = 0
         }
         
         if operation == 4{
             number = number2 / number
             doubleToInt()
             operation = 0
+            myLabel.tag = 0
         }
         
     }
